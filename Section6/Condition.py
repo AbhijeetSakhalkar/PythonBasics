d0af3c648e46089bf270f0c0390da9b67824c7cd
Abhijeet Sakhalkar <sakhalkar123abj@gmail.com>
2018-10-03 19:42:38 +0530

    section 6

---
+++ Section6/Condition.py
@@ -0,0 +1,15 @@
+a = "abhijeet"
+b = "abhijeet"
+
+if a == b:
+    print("yes")
+else:
+    print("no")
+
+
+c =13
+
+if c%2 ==0:
+    print("yes")
+else:
+    print("no")
---
+++ Section6/Loops.py
@@ -0,0 +1,7 @@
+for i in range(3):
+    print("i= "+str(i)) # i must be a string, otherwise an exception will be thrown
+
+j=1
+while(j<6):
+    print("j= "+str(j))
+    j +=1
