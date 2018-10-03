609e30e46ffa2036006c026444de8c0e189c8d5c
Abhijeet Sakhalkar <Abhijeet.Sakhalkar@sas.com>
2018-10-03 11:27:00 +0530

    till section 3

---
+++ Section3/VariablesAndComments.py
@@ -0,0 +1,14 @@
+# Comments and Variables
+# Currently as of 3rd oct 2018 there is no multi line comments present in python
+
+# Below are the 4 types of variables that python support
+
+x = 100
+y = 100.385
+z = "Words or Letters"
+a = False
+
+print(type(a))
+print(type(x))
+print(type(y))
+print(type(z))
